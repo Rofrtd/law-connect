3e96cebf64b6786a73dc792afdceb2f8849f6aa1
Romy Furtado <rofrtd@gmail.com>
2025-12-16 23:52:37 +1100

    feat(e2e): use separate db

--- src/db/client.ts
+++ src/db/client.ts
@@ -3,7 +3,7 @@ import { drizzle } from "drizzle-orm/better-sqlite3";
 import { migrate } from "drizzle-orm/better-sqlite3/migrator";
 import path from "node:path";
 
-export function createDb(filename: string = "db.sqlite") {
+export function createDb(filename = process.env.SQLITE_PATH ?? "db.sqlite") {
   const sqlite = new Database(filename);
   const db = drizzle(sqlite);
 
